d5ec08e8f7dad8ace0ce3309c92759d3e264334a
daten-shi55 <richardavillanosa@gmail.com>
2024-11-15 22:05:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const audio = document.getElementById('background-audio');
     audio.volume = 0.7; 
 
-audio.play(); // Explicitly play the audio
-}); 
-        }
-    });
-});
+           audio.play(); // 
+   }); 
+        
